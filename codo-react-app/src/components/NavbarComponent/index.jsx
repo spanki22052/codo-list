5b56fab3498595c8d84b11b1407cd15408287309
spanki22052 <useful22052@gmail.com>
2020-06-26 17:16:09 +0300

    added login save system

--- codo-react-app/src/components/NavbarComponent/index.jsx
+++ codo-react-app/src/components/NavbarComponent/index.jsx
@@ -6,11 +6,22 @@ import {
   montserratFont,
   NavLinkStyle,
 } from "./styles";
+const firebase = require("firebase");
 
 class NavbarComponent extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: "",
+      todos: [],
+    };
+  }
+
   render() {
     return (
       <div className="navbar-component">
+        {console.log(this.state.email)}
         <Navbar style={darkBackground} expand="lg">
           <Navbar.Brand
             style={{
@@ -23,7 +34,6 @@ class NavbarComponent extends Component {
             CodoList
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
-
           <Navbar.Collapse
             id="basic-navbar-nav"
             className="justify-content-end"
@@ -31,14 +41,48 @@ class NavbarComponent extends Component {
             <Nav.Link style={NavLinkStyle} href="/">
               home
             </Nav.Link>
-            <Nav.Link style={NavLinkStyle} href="/login">
-              login
-            </Nav.Link>
+            {this.state.email.length > 0 ? (
+              <Nav.Link
+                onClick={this.firebaseSignOut}
+                style={NavLinkStyle}
+                href="/"
+              >
+                signout
+              </Nav.Link>
+            ) : (
+              <Nav.Link style={NavLinkStyle} href="/signup">
+                signup
+              </Nav.Link>
+            )}
           </Navbar.Collapse>
         </Navbar>
       </div>
     );
   }
+
+  firebaseSignOut = () => {
+    firebase.auth().signOut();
+    this.setState({ isLoggedIn: false });
+  };
+
+  componentWillMount = () => {
+    firebase.auth().onAuthStateChanged(async (_usr) => {
+      if (!_usr) console.log("You aren't logged in");
+      else {
+        await firebase
+          .firestore()
+          .collection("todo")
+          .where("users", "array-contains", _usr.email)
+          .onSnapshot(async (res) => {
+            const todos = res.docs.map((_doc) => _doc.data());
+            await this.setState({
+              todos: todos,
+              email: _usr.email,
+            });
+          });
+      }
+    });
+  };
 }
 
 export default NavbarComponent;
--- codo-react-app/src/components/index.jsx
+++ codo-react-app/src/components/index.jsx
@@ -5,6 +5,7 @@ import LoginPage from "./LoginPage";
 import RegisterPage from "./RegisterPage";
 import MainPage from "./Mainpage";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Navbar } from "react-bootstrap";
 
 const firebase = require("firebase");
 
@@ -28,9 +29,9 @@ const ComponentsHolder = () => {
       <div className="components-holder">
         <NavbarComponent />
         <Switch>
-          <Route exact path="/" component={MainPage} />
-          <Route exact path="/login" component={LoginPage} />
-          <Route exact path="/register" component={RegisterPage} />
+            <Route exact path="/" component={MainPage} />
+            <Route path="/login" component={LoginPage} />
+            <Route path="/register" component={RegisterPage} />
         </Switch>
       </div>
     </Router>
