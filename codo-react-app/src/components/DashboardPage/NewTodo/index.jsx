2ec4a58505b054565e6b81c96c27f3c2c149e1bd
spanki22052 <useful22052@gmail.com>
2020-06-27 17:44:55 +0300

    create todo add function

---
+++ codo-react-app/src/components/DashboardPage/NewTodo/index.jsx
@@ -0,0 +1,81 @@
+import React, { Component } from "react";
+import "./new.scss";
+const firebase = require("firebase");
+
+class NewTodo extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todoInput: "",
+      descriptionInput: "",
+    };
+  }
+
+  render() {
+    return (
+      <div style={{ display: this.props.showNew }} className="newtodo">
+        <div className="input-block">
+          <p>Task:</p>
+          <input
+            value={this.state.todoInput}
+            onChange={(e) => this.setState({ todoInput: e.target.value })}
+            type="text"
+          />
+        </div>
+        <div className="input-block">
+          <p>Description:</p>
+          <textarea
+            value={this.state.descriptionInput}
+            onChange={(e) =>
+              this.setState({ descriptionInput: e.target.value })
+            }
+            type="text"
+          />
+        </div>
+        <div className="buttons-side">
+          <button onClick={this.props.changeDisplay} className="close-btn">
+            Close
+          </button>
+          <button onClick={this.createNewTodo} className="ready-btn">
+            Ready
+          </button>
+        </div>
+      </div>
+    );
+  }
+
+  createNewTodo = () => {
+    this.state.todoInput.length > 0 && this.state.descriptionInput.length > 0
+      ? firebase
+          .firestore()
+          .collection("todo")
+          .doc(this.props.email)
+          .set({
+            todolist: [
+              ...this.props.todos,
+              {
+                description: this.state.descriptionInput,
+                isFinished: false,
+                percentage: 0,
+                todo: this.state.todoInput,
+              },
+            ],
+          })
+          .then(
+            (dbError) => {
+              console.log(dbError);
+              this.setState({ signupError: "Failed to load user" });
+            }
+          )
+      : console.log("can't create");
+    this.props.addTodo({
+      description: this.state.descriptionInput,
+      isFinished: false,
+      percentage: 0,
+      todo: this.state.todoInput,
+    });
+    this.setState({ todoInput: "", descriptionInput: "" });
+  };
+}
+
+export default NewTodo;
--- codo-react-app/src/components/DashboardPage/index.jsx
+++ codo-react-app/src/components/DashboardPage/index.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./dashboard.scss";
+import NewTodo from "./NewTodo";
 const firebase = require("firebase");
 
 class DasboardPageComponent extends Component {
@@ -8,6 +9,7 @@ class DasboardPageComponent extends Component {
     this.state = {
       todos: [],
       email: "",
+      showNew: "none",
     };
   }
 
@@ -37,20 +39,70 @@ class DasboardPageComponent extends Component {
       }
     });
   };
+
+  colorCheck = (percentage) => {
+    if (percentage < 35 && percentage === 0) {
+      return "#0DC160";
+    } else if (percentage < 60) {
+      return "#FCD400";
+    } else {
+      return "#FE0000";
+    }
+  };
+
   render() {
     return (
       <div className="dashboard-page">
+        <NewTodo
+          showNew={this.state.showNew}
+          todos={this.state.todos}
+          email={this.state.email}
+          addTodo={(newTodo) =>
+            this.setState({ todos: [...this.state.todos, newTodo] })
+          }
+          changeDisplay={() => this.setState({ showNew: "none" })}
+        />
         {this.state.todos.length > 0 ? (
           <div className="todo-blocks">
-            <div className="todo-block">
-              <div className="processbar-side">
-                <button className="edit-button">Edit</button>
-                <button className="finish-button">Finish</button>
-              </div>
-              <div className="text-side">
-                <h1>Start to work on CodoList</h1>
-                <p>Create some todo task on CodoList application</p>
-              </div>
+            {this.state.todos.map((element, index) => {
+              let percentageCounter = 440 + element.percentage * 2 + 10;
+              return (
+                <div key={index} className="todo-block">
+                  <div className="processbar-side">
+                    <div className="percent">
+                      <svg>
+                        <circle cx="35" cy="35" r="35"></circle>
+                        <circle
+                          cx="35"
+                          cy="35"
+                          r="35"
+                          id="to-change"
+                          style={{
+                            strokeDashoffset: `-${percentageCounter}`,
+                            stroke: `${this.colorCheck(element.percentage)}`,
+                          }}
+                        ></circle>
+                      </svg>
+                      <div className="number">
+                        <h2>
+                          {element.percentage} <span>%</span>
+                        </h2>
+                      </div>
+                    </div>
+                    <button className="edit-button">Edit</button>
+                    <button className="finish-button">Finish</button>
+                  </div>
+                  <div className="text-side">
+                    <h1>{element.todo}</h1>
+                    <p>{element.description}</p>
+                  </div>
+                </div>
+              );
+            })}
+            <div className="new-block">
+              <button onClick={() => this.setState({ showNew: "block" })}>
+                +
+              </button>
             </div>
           </div>
         ) : (
@@ -59,7 +111,9 @@ class DasboardPageComponent extends Component {
               <h1 className="gray-text">You don't have any tasks for today</h1>
             </div>
             <div className="gray-box">
-              <button>+</button>
+              <button onClick={() => this.setState({ showNew: "block" })}>
+                +
+              </button>
             </div>
           </div>
         )}
